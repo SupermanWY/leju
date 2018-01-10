4e82e20127de9dfe1eca1e050b7fb2a25e670e8b
Dream <1104655540@qq.com>
2018-01-10 11:24:20 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/home/index'
-
+import Register from '@/pages/home/register'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
